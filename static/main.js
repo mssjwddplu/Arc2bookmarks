d14058ef78d53977d8d9635e966f4a8b42af1234
mssjwddplu <mssjwddp@163.com>
2023-08-22 08:38:03 +0800

    index增加等待时间div，main.js大幅修改，使用jQuery的AJAX方法替代fetch，加入上传文件时显示估计的等待时间的功能

--- static/main.js
+++ static/main.js
@@ -1,36 +1,47 @@
-function convertToJson() {
-    const fileInput = document.getElementById('jsonInput');
-    const file = fileInput.files[0];
-    if (!file) {
-        alert('Please select a JSON file first.');
-        return;
-    }
-
-    const formData = new FormData();
-    formData.append('json', file);
-
-    // 打印要发送的文件信息
-    console.log("Sending file:", file.name, "Size:", file.size);
-
-    // 修改这里的 URL
-    fetch('/api/importArc2html', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`Server responded with an error. Status: ${response.status}`);
+$(document).ready(function() {
+    $("#upload-form").submit(function(e) {
+        e.preventDefault();
+        
+        const fileInput = document.getElementById('jsonInput');
+        const file = fileInput.files[0];
+        if (!file) {
+            alert('Please select a JSON file first.');
+            return;
         }
-        return response.blob();
-    })
-    .then(blob => {
-        const downloadLink = document.getElementById('downloadLink');
-        downloadLink.href = URL.createObjectURL(blob);
-        downloadLink.download = 'converted.html';
-        downloadLink.style.display = 'block';
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('An error occurred. Please try again.');
+
+        const formData = new FormData();
+        formData.append('json', file);
+
+        // 打印要发送的文件信息
+        console.log("Sending file:", file.name, "Size:", file.size);
+
+        $.ajax({
+            url: '/api/importArc2html',
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            beforeSend: function() {
+                // 显示估计的等待时间
+                const estimatedTime = file_queue_count * 2;  // 假设每个文件处理需要2秒
+                $("#estimated-wait-time").text("Estimated wait time: " + estimatedTime + " seconds");
+            },
+            success: function(data) {
+                const downloadLink = document.getElementById('downloadLink');
+                downloadLink.href = URL.createObjectURL(data);
+                downloadLink.download = 'converted.html';
+                downloadLink.style.display = 'block';
+
+                // 隐藏估计的等待时间
+                $("#estimated-wait-time").text("");
+            },
+            error: function(jqXHR, textStatus, errorMessage) {
+                console.error('Error:', errorMessage);
+                alert('An error occurred. Please try again.');
+
+                // 隐藏估计的等待时间
+                $("#estimated-wait-time").text("");
+            }
+        });
     });
-}
+});
